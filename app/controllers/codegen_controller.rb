d2fd4207718f46520e99bd8dbcdde34433174900
Reed Perkins <reed@reedperkins.com>
2024-09-13 08:00:53 -0600

    Add support for template type

--- app/controllers/codegen_controller.rb
+++ app/controllers/codegen_controller.rb
@@ -1,13 +1,14 @@
 class CodegenController < ApplicationController
   def new
     @prompt = params[:prompt]
+    @component_type = params[:component_type]
     @html_code = params[:html_code]
   end
 
   def create
     @prompt = params[:prompt]
-    @html_code = chatgpt.send_message(@prompt)
-    # @html_code = "<p>a paragraph</p>"
+    @component_type = params[:component_type]
+    @html_code = chatgpt.send_message(@prompt, template(@component_type))
     render :new
   end
 
@@ -16,4 +17,31 @@ class CodegenController < ApplicationController
   def chatgpt
     @chatgpt ||= ChatGptHelper.new
   end
+
+  def template(type)
+    # You could modify this to use the class you wrote
+    return card_template if type == "card"
+    return form_template if type == "form"
+    return table_template if type == "table"
+  end
+
+  def card_template
+    <<~HTML
+      // Matt, insert html for the template here
+    HTML
+  end
+
+  def table_template
+    <<~HTML
+
+    HTML
+  end
+
+  def form_template
+    <<~HTML
+      
+    HTML
+  end
+
+
 end
--- lib/chat_gpt_helper.rb
+++ lib/chat_gpt_helper.rb
@@ -1,21 +1,32 @@
 class ChatGptHelper
   def initialize(api_key = Rails.application.credentials.dig(:openai, :api_key))
-    @api_key = api_key
-  end
-
-  def send_message(message)
-    message = { role: "user", content: message }
-    client = OpenAI::Client.new(
-      access_token: @api_key,
+    @client = OpenAI::Client.new(
+      access_token: api_key,
       log_errors: true
     )
-    response = client.chat(
+  end
+
+  def send_message(content, template)
+    response = @client.chat(
       parameters: {
         model: "gpt-3.5-turbo",
-        messages: [message],
-        temperature: 0.7,
+        messages: messages(content, template),
       }
     )
+
     response.dig("choices", 0, "message", "content")
+  end
+
+  def messages(content, template)
+    [
+      { role: "system", content: system_prompt(template) },
+      { role: "user", content: }
+    ]
+  end
+
+  def system_prompt(template)
+    "You are an assistant that generates HTML snippets based on user prompts that conforms to a template. " \
+      "The template is as follows: #{template}. Output new HTML code that conforms to the template outline." \
+      "You should only generate HTML code. Do not apply additional formatting to the HTML code."
   end
 end
